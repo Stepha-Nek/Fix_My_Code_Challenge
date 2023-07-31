2a95712b285fb864cdd03f56bbd018d5ccb64f74
Stepha-Nek <Nneka.oguh1@gmail.com>
2023-07-30 17:36:16 -0700

    ruby

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -11,43 +11,45 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *saved_head;
-	dlistint_t *tmp;
-	unsigned int p;
+    if (*head == NULL) // Check if the list is empty
+    {
+        return (-1); // Return -1 to indicate failure (empty list)
+    }
 
-	if (*head == NULL)
-	{
-		return (-1);
-	}
-	saved_head = *head;
-	p = 0;
-	while (p < index && *head != NULL)
-	{
-		*head = (*head)->next;
-		p++;
-	}
-	if (p != index)
-	{
-		*head = saved_head;
-		return (-1);
-	}
-	if (0 == index)
-	{
-		tmp = (*head)->next;
-		free(*head);
-		*head = tmp;
-		if (tmp != NULL)
-		{
-			tmp->prev = NULL;
-		}
-	}
-	else
-	{
-		(*head)->prev->prev = (*head)->prev;
-		free(*head);
-		if ((*head)->next)
-			(*head)->next->prev = (*head)->prev;
-		*head = saved_head;
-	}
-	return (1);
+    dlistint_t *saved_head;
+    dlistint_t *tmp;
+    unsigned int p;
+
+    saved_head = *head;
+    p = 0;
+    while (p < index && *head != NULL)
+    {
+        *head = (*head)->next;
+        p++;
+    }
+    if (p != index)
+    {
+        *head = saved_head;
+        return (-1);
+    }
+    if (0 == index)
+    {
+        tmp = (*head)->next;
+        free(*head);
+        *head = tmp;
+        if (tmp != NULL)
+        {
+            tmp->prev = NULL;
+        }
+    }
+    else
+    {
+        (*head)->prev->prev = (*head)->prev;
+        free(*head);
+        if ((*head)->next)
+            (*head)->next->prev = (*head)->prev;
+        *head = saved_head;
+    }
+    return (1);
 }
+
