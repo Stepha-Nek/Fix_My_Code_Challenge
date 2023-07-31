799d3b4cba31290581696cac54cb96749087e6af
Stepha-Nek <Nneka.oguh1@gmail.com>
2023-07-30 17:55:26 -0700

    c

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -11,45 +11,44 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    if (*head == NULL) // Check if the list is empty
-    {
-        return (-1); // Return -1 to indicate failure (empty list)
-    }
+	dlistint_t *saved_head;
+	dlistint_t *tmp;
+	unsigned int p;
 
-    dlistint_t *saved_head;
-    dlistint_t *tmp;
-    unsigned int p;
-
-    saved_head = *head;
-    p = 0;
-    while (p < index && *head != NULL)
-    {
-        *head = (*head)->next;
-        p++;
-    }
-    if (p != index)
-    {
-        *head = saved_head;
-        return (-1);
-    }
-    if (0 == index)
-    {
-        tmp = (*head)->next;
-        free(*head);
-        *head = tmp;
-        if (tmp != NULL)
-        {
-            tmp->prev = NULL;
-        }
-    }
-    else
-    {
-        (*head)->prev->prev = (*head)->prev;
-        free(*head);
-        if ((*head)->next)
-            (*head)->next->prev = (*head)->prev;
-        *head = saved_head;
-    }
-    return (1);
+	if (*head == NULL)
+	{
+		return (-1);
+	}
+	saved_head = *head;
+	p = 0;
+	while (p < index && *head != NULL)
+	{
+		*head = (*head)->next;
+		p++;
+	}
+	if (p != index)
+	{
+		*head = saved_head;
+		return (-1);
+	}
+	if (0 == index)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+		if (tmp != NULL)
+		{
+			tmp->prev = NULL;
+		}
+	}
+	else
+	{
+		(*head)->prev->next = (*head)->next;
+		free(*head);
+		if ((*head)->next)
+			(*head)->next->prev = (*head)->prev;
+		*head = saved_head;
+	}
+	return (1);
 }
 
